a2a6eeb502191c1f45a51958295d9e825449e928
ChrisMagnuson <hourglasssand@gmail.com>
2016-07-20 14:01:14 -0400

    Initial commit

---
+++ TervisPowerShellBPMN.psm1
@@ -0,0 +1,15 @@
+﻿function Get-BPMNADUserSAMAccountNameFromName {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory)][string]$Name
+    )
+
+    $SAMAccountName = Get-ADUser -Filter {name -eq $Name } | 
+    select -ExpandProperty SAMAccountName
+
+    if ($SAMAccountName.count -eq 1) {
+        [pscustomobject]@{SAMAccountName = $SAMAccountName}
+    } else {
+        Throw "Searching for $Name returned $SAMAccountName (None or more than one samaccountname)"
+    }
+}
