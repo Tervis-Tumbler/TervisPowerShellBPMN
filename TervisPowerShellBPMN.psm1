43ec4bf1fc31e5f39c90e4dc4cf34f3ea37ec3e8
ChrisMagnuson <hourglasssand@gmail.com>
2016-07-21 16:48:20 -0400

    Get-BPMNEmployeeOnlyInMES

--- TervisPowerShellBPMN.psm1
+++ TervisPowerShellBPMN.psm1
@@ -13,3 +13,21 @@
         Throw "Searching for $Name returned $SAMAccountName (None or more than one samaccountname)"
     }
 }
+
+function Get-BPMNEmployeeOnlyInMES {
+    param(
+        $Username
+    )
+    $ADUser = get-aduser -Identity $UserName -Properties emailaddress,memberof,PrimaryGroup
+    $ADUserMemberOfGroupNames = $ADUser.MemberOf | Get-ADGroup | select -ExpandProperty Name
+    $ADUserMemberOfGroupNames += $ADUser.PrimaryGroup | Get-ADGroup | select -ExpandProperty Name
+
+    $EmployeeOnlyInMES = $ADUser | 
+    where emailaddress -eq $null |
+    where {$ADUserMemberOfGroupNames.count -eq 3} |
+    where {$ADUserMemberOfGroupNames -Contains "Domain Users"} |
+    where {$ADUserMemberOfGroupNames -Contains "Emblem Placement Users"} |
+    where {$ADUserMemberOfGroupNames -Contains "Electronic Packing Station Users"}
+
+    [pscustomobject]@{EmployeeOnlyInMES = if($EmployeeOnlyInMES) {$true} else {$false} }
+}
