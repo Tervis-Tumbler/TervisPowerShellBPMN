b36b788043a629824040eeac2ee5cbb2347c55d9
ChrisMagnuson <hourglasssand@gmail.com>
2016-09-23 23:59:35 -0400

    New Test-BPMNUsernameAllowedToAccessMES and Get-ManagerSamAccountNameFromEmployeeSamAccountName functions

--- TervisPowerShellBPMN.psm1
+++ TervisPowerShellBPMN.psm1
@@ -14,6 +14,39 @@
     }
 }
 
+function Test-BPMNUsernameAllowedToAccessMES {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory)][string]$Username
+    )
+
+    $ADUser = get-aduser -Identity $Username -Properties memberof,PrimaryGroup
+    $ADUserMemberOfGroupNames = $ADUser.MemberOf | Get-ADGroup | select -ExpandProperty Name
+    $ADUserMemberOfGroupNames += $ADUser.PrimaryGroup | Get-ADGroup | select -ExpandProperty Name
+
+    $EmployeeInMES = $ADUser | 
+    where {$ADUserMemberOfGroupNames -Contains "Emblem Placement Users"} |
+    where {$ADUserMemberOfGroupNames -Contains "Electronic Packing Station Users"}
+
+    [pscustomobject]@{EmployeeInMES = if($EmployeeInMES) {$true} else {$false} }
+}
+
+Function Add-BPMNMESUserToOverrideGroup {
+    param(
+        [parameter(Mandatory)][string]$Username
+    )
+    $ADUser = get-aduser -Identity $Username -Properties memberof,PrimaryGroup
+    Get-ADGroup -Identity "MES Leaders" | Add-ADGroupMember -Members $ADUser
+}
+
+Function Remove-BPMNMESUserFromOverrideGroup {
+    param(
+        [parameter(Mandatory)][string]$Username
+    )
+    $ADUser = get-aduser -Identity $Username -Properties memberof,PrimaryGroup
+    Get-ADGroup -Identity "MES Leaders" | Remove-ADGroupMember -Members $ADUser -Confirm:$false
+}
+
 function Get-BPMNEmployeeOnlyInMES {
     param(
         $Username
@@ -30,4 +63,18 @@ function Get-BPMNEmployeeOnlyInMES {
     where {$ADUserMemberOfGroupNames -Contains "Electronic Packing Station Users"}
 
     [pscustomobject]@{EmployeeOnlyInMES = if($EmployeeOnlyInMES) {$true} else {$false} }
+}
+
+function Get-ManagerSamAccountNameFromEmployeeSamAccountName {
+    param(
+        $EmployeeSamAccountName
+    )
+    $EmployeeADUser = Get-ADUser $EmployeeSamAccountName -Properties manager
+    $ManagerSamAccountName = get-aduser $EmployeeADUser.Manager | select -ExpandProperty SamAccountName
+
+    if ($ManagerSamAccountName.count -eq 1) {
+        [pscustomobject]@{ManagerUsername = $ManagerSamAccountName }
+    } else {
+        Throw "Searching for the manager of $EmployeeSamAccountName returned $ManagerSamAccountName (None or more than one samaccountname)"
+    }
 }
